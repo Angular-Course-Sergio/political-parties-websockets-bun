ab4374e6a6fa4968bc9e8757c1d23aabae812b3c
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-01-31 21:53:26 -0600

    feat: Add reactive chart data update mechanism in BarChart component

--- src/app/live-chart/components/bar-chart/bar-chart.ts
+++ src/app/live-chart/components/bar-chart/bar-chart.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, input, OnDestroy, OnInit, viewChild } from '@angular/core';
+import { Component, effect, ElementRef, input, OnDestroy, OnInit, viewChild } from '@angular/core';
 import { Chart, ChartData } from 'chart.js';
 
 @Component({
@@ -10,6 +10,18 @@ export class BarChart implements OnInit, OnDestroy {
   private readonly canvasRef = viewChild<ElementRef<HTMLCanvasElement>>('chart');
   private chartInstance: Chart | null = null;
   public chartData = input.required<ChartData<'bar'>>();
+
+  private updateChartData = effect(() => {
+    if (!this.chartInstance) return;
+
+    this.chartInstance.data.labels = this.chartData().labels;
+    this.chartInstance.data.datasets[0].data = this.chartData().datasets[0].data;
+    this.chartInstance.data.datasets[0].backgroundColor =
+      this.chartData().datasets[0].backgroundColor;
+    this.chartInstance.data.datasets[0].borderColor = this.chartData().datasets[0].borderColor;
+
+    this.chartInstance.update();
+  });
 
   ngOnInit(): void {
     const canvas = this.canvasRef()?.nativeElement;
